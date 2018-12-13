98770df0f0d3f6b2622a6bccc131a2e8ec81afa1
Antoine Jubin <antoine.jubin@gmail.com>
2018-12-13 18:56:13 +0100

    WIP:add some tests

---
+++ test/Store/Reducers/favoriteReducer.js
@@ -0,0 +1,7 @@
+import reducer from "../../../Store/Reducers/favoriteReducer";
+import expect from "expect";
+
+describe("favorite reducer", () => {
+  it("should return the initial state");
+  it("should handle TOGGLE_FAVORITE");
+});
